203b68271be13067f22f277df49ea4c182a957ab
ATkingma <60423088+ATkingma@users.noreply.github.com>
2021-04-07 17:57:17 +0200

    begin path finding

---
+++ Survival game/Assets/Scripts/pathfinding/Grid.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public Transform startPosition;
+    public LayerMask wallMask;
+    public Vector2 gridWorldSize;
+    public float nodeRadius;
+    public float distance;
+
+    Node[,] grid;
+    public List<Node> finalPath;
+
+    public float nodeDiameter;
+    public int gridSizeX;
+    public int gridSizeY;
+
+    private void Start()
+    {
+        nodeDiameter = nodeRadius * 2;
+        gridSizeX = Mathf.RoundToInt(gridWorldSize.x/nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+
+        CreateGrid();
+    }
+    public void CreateGrid()
+    {
+        grid = new Node[gridSizeX, gridSizeY];
+        Vector3 bottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2; 
+        for (int x = 0; x < gridSizeX; x++)
+        {
+
+        }
+    }
+}
---
+++ Survival game/Assets/Scripts/pathfinding/MoveTargetBlock.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class MoveTargetBlock : MonoBehaviour
+{
+    // public
+    public LayerMask hitlayers;
+    void Update()
+    {
+        if (Input.GetButtonDown("fire1"))
+        {
+            Vector3 mouse = Input.mousePosition;
+            Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+            RaycastHit hit;
+            if(Physics.Raycast(castPoint,out hit, Mathf.Infinity, hitlayers))
+            {
+                this.transform.position = hit.point;
+            }
+        }
+    }
+}
